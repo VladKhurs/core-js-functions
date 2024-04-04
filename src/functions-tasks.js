ab5faa698f0fb8f1df8b72906735697098892840
Vladislav Khurs <130601125+VladKhurs@users.noreply.github.com>
2024-04-04 09:44:34 +0200

    feat: update functions-tasks.js

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -18,7 +18,7 @@
  *
  */
 function getCurrentFunctionName() {
-  throw new Error('Not implemented');
+  return getCurrentFunctionName.name;
 }
 
 /**
@@ -50,8 +50,12 @@ function getFunctionBody(/* func */) {
  *  ]) => [0, 1, 2]
  *
  */
-function getArgumentsCount(/* funcs */) {
-  throw new Error('Not implemented');
+function getArgumentsCount(funcs) {
+  const ans = [];
+  for (let i = 0; i < funcs.length; i += 1) {
+    ans.push(funcs[i].length);
+  }
+  return ans;
 }
 
 /**
@@ -70,8 +74,10 @@ function getArgumentsCount(/* funcs */) {
  *   power05(16) => 4
  *
  */
-function getPowerFunction(/* exponent */) {
-  throw new Error('Not implemented');
+function getPowerFunction(exponent) {
+  return function n(x) {
+    return x ** exponent;
+  };
 }
 
 /**
@@ -105,8 +111,16 @@ function getPolynom() {
  *   ...
  *   memoizer() => the same random number  (next run, returns the previous cached result)
  */
-function memoize(/* func */) {
-  throw new Error('Not implemented');
+function memoize(func) {
+  let isCalled = false;
+  let data = 0;
+  return function d() {
+    if (!isCalled) {
+      isCalled = true;
+      data = func();
+    }
+    return data;
+  };
 }
 
 /**
@@ -168,8 +182,10 @@ function logger(/* func, logFunc */) {
  *   partialUsingArguments(fn, 'a','b','c')('d') => 'abcd'
  *   partialUsingArguments(fn, 'a','b','c','d')() => 'abcd'
  */
-function partialUsingArguments(/* fn, ...args1 */) {
-  throw new Error('Not implemented');
+function partialUsingArguments(fn, ...args1) {
+  return function n(...args) {
+    return fn(...args1, ...args);
+  };
 }
 
 /**
